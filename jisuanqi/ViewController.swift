871781d074fd951000251640a82b4da6a21de6cc
20161104612 <643024265@qq.com>
2018-09-28 17:25:55 +0800

    jsq1
    display 1，2，。。。

--- jisuanqi/ViewController.swift
+++ jisuanqi/ViewController.swift
@@ -10,6 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBAction func bu(_ sender: Any) {
+       test.text = "0"
+    }
+    
+    @IBAction func bu1(_ sender: Any) {
+        test.text = "0"
+    }
+    
+    @IBAction func bu2(_ sender: Any) {
+        test.text = "1"
+    }
+    
+    @IBAction func bu3(_ sender: Any) {
+        test.text = "2"
+    }
+    
+    @IBAction func bu4(_ sender: Any) {
+        test.text = "3"
+    }
+    
+    @IBAction func bu5(_ sender: Any) {
+        test.text = "4"
+    }
+    
+    @IBAction func bu6(_ sender: Any) {
+        test.text = "5"
+    }
+    
+    @IBAction func bu7(_ sender: Any) {
+        test.text = "6"
+    }
+    
+    @IBAction func bu8(_ sender: Any) {
+        test.text = "7"
+    }
+    
+    @IBAction func bu9(_ sender: Any) {
+        test.text = "8"
+    }
+    
+    @IBAction func bu10(_ sender: Any) {
+        test.text = "9"
+    }
+    
+    @IBOutlet weak var test:
+    UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
