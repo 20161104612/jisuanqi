6e97f24d746e00125fade268c4d897c433284d06
20161104612 <643024265@qq.com>
2018-10-10 11:57:01 +0800

    计算器
    2

--- jisuanqi/ViewController.swift
+++ jisuanqi/ViewController.swift
@@ -9,54 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-
-    @IBAction func bu(_ sender: Any) {
-       test.text = "0"
-    }
     
-    @IBAction func bu1(_ sender: Any) {
-        test.text = "0"
-    }
+    @IBOutlet weak var test: UITextField!
+    var userIsTheMiddleOfTypingANumber = false
     
-    @IBAction func bu2(_ sender: Any) {
-        test.text = "1"
+    @IBAction func app(_ sender: AnyObject) {
+        let digit = sender.currentTitle!!
+        if userIsTheMiddleOfTypingANumber
+        {
+            test.text = test.text! + digit
+        }
+        else
+        {
+            test.text = digit
+            userIsTheMiddleOfTypingANumber = true
+        }
     }
-    
-    @IBAction func bu3(_ sender: Any) {
-        test.text = "2"
-    }
-    
-    @IBAction func bu4(_ sender: Any) {
-        test.text = "3"
-    }
-    
-    @IBAction func bu5(_ sender: Any) {
-        test.text = "4"
-    }
-    
-    @IBAction func bu6(_ sender: Any) {
-        test.text = "5"
-    }
-    
-    @IBAction func bu7(_ sender: Any) {
-        test.text = "6"
-    }
-    
-    @IBAction func bu8(_ sender: Any) {
-        test.text = "7"
-    }
-    
-    @IBAction func bu9(_ sender: Any) {
-        test.text = "8"
-    }
-    
-    @IBAction func bu10(_ sender: Any) {
-        test.text = "9"
-    }
-    
-    @IBOutlet weak var test:
-    UITextField!
     
     
     override func viewDidLoad() {
